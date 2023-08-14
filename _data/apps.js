2c6d5b7b7d7223ae9908b4669963481bd6593ec4
QuangSon <quangson91@gmail.com>
2023-08-14 16:17:29 +0700

    Update metadata

---
+++ _data/apps.js
@@ -0,0 +1,8 @@
+module.exports = [
+  {
+    name: "Smart CSV: CSV File Viewer",
+    description: "More than a Csv File Viewer, you can generate PDF, create chart and even using SQL query to query your data.",
+    android: "https://play.google.com/store/apps/details?id=xyz.minimalistapps.smartcsv",
+    "ios": "https://apps.apple.com/us/app/smart-csv-viewer-smart-tables/id1611962123"
+  },
+];
---
+++ _data/info.js
@@ -0,0 +1,6 @@
+module.exports = {
+  "current_year": new Date().getFullYear(),
+  "description": "We build minimalist apps for everyone.",
+  "url": "https://minimalistapps.github.io",
+  "image_url": "https://minimalistapps.github.io/icon-512.png"
+};
