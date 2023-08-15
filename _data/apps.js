f737e3bbdef59ed4163b8fcbf3948f6b6e6f67ef
QuangSon <quangson91@gmail.com>
2023-08-15 09:20:26 +0700

    Update

--- _data/apps.js
+++ _data/apps.js
@@ -1,8 +1,19 @@
 module.exports = [
   {
     name: "Smart CSV: CSV File Viewer",
-    description: "More than a Csv File Viewer, you can generate PDF, create chart and even using SQL query to query your data.",
+    summary: "Effortlessly explore CSVs: PDFs/charts, filters, SQL queries, and more",
+    description: [
+      "More than a basic document reader, it's your comprehensive CSV file explorer. Uncover all this with Smart CSV Viewer:",
+      "- Easy to view your CSV file.",
+      "- Analyzing your data is easy by using visual filters or SQL queries.",
+      "- Generate a chart image.",
+      "- Generate a pdf file. A CSV file can be custom data and style to export to a pdf file as you want.",
+      "- Manage all the exported files and share them with your friends.",
+      "- Easy to search your content.",
+      "- Copy the selected rows.",
+      "- and more.",
+    ],
     android: "https://play.google.com/store/apps/details?id=xyz.minimalistapps.smartcsv",
-    "ios": "https://apps.apple.com/us/app/smart-csv-viewer-smart-tables/id1611962123"
+    ios: "https://apps.apple.com/us/app/smart-csv-viewer-smart-tables/id1611962123"
   },
 ];
